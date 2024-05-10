8cab9e182a7cb6f2f9c6daf37a1ccb45ad5dd271
Kyle-OD <kyleodonnell@comcast.net>
2024-05-09 23:27:58 -0400

    environment and model weights

--- priority_prediction/train_ppo_priority_scheduler.py
+++ priority_prediction/train_ppo_priority_scheduler.py
@@ -1,30 +1,49 @@
 import numpy as np
 import torch
+import time, datetime
 
 import gymnasium as gym
 import gym_env
 
 from ppo import PPO
 
-dataset1 = np.genfromtxt("./dataset/test.csv", delimiter=',', skip_header=1)
-dataset2 = np.genfromtxt("./dataset/test.csv", delimiter=',', skip_header=1)
-dataset3 = np.genfromtxt("./dataset/test.csv", delimiter=',', skip_header=1)
-dataset4 = np.genfromtxt("./dataset/test.csv", delimiter=',', skip_header=1)
-dataset5 = np.genfromtxt("./dataset/test.csv", delimiter=',', skip_header=1)
+dataset1 = np.genfromtxt("./dataset/dataset1.csv", delimiter=',', skip_header=1)
+dataset2 = np.genfromtxt("./dataset/dataset2.csv", delimiter=',', skip_header=1)
+dataset3 = np.genfromtxt("./dataset/dataset3.csv", delimiter=',', skip_header=1)
+dataset4 = np.genfromtxt("./dataset/dataset4.csv", delimiter=',', skip_header=1)
+dataset5 = np.genfromtxt("./dataset/dataset5.csv", delimiter=',', skip_header=1)
 
 env = gym.make("gym_env:gym_env/PriorityScheduler-v0", data=dataset1, encoder_context=10, max_priority=10)
 model = PPO(env, 64)
 
-n_steps = 5
+n_steps = 10000
 
+print('Training PPO model with', n_steps, 'per dataset')
+start_time = time.time()
+
+print('Starting time:', datetime.datetime.now())
 model.learn(n_steps)
+print('Training on first dataset complete after', time.time() - start_time, 'seconds')
+
+start_time = time.time()
 env.reset(options={'new_data': dataset2})
 model.learn(n_steps)
+print('Training on second dataset complete after', time.time() - start_time, 'seconds')
+
+start_time = time.time()
 env.reset(options={'new_data': dataset3})
 model.learn(n_steps)
+print('Training on third dataset complete after', time.time() - start_time, 'seconds')
+
+start_time = time.time()
 env.reset(options={'new_data': dataset4})
 model.learn(n_steps)
+print('Training on fourth dataset complete after', time.time() - start_time, 'seconds')
+
+start_time = time.time()
 env.reset(options={'new_data': dataset5})
 model.learn(n_steps)
+print('Training on fifth dataset complete after', time.time() - start_time, 'seconds')
+start_time = time.time()
 
 torch.save(model.actor.state_dict(), 'model_weights/ml_priority_scheduler.pt')
